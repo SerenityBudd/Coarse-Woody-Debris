01f2c073fd0f2b1e52ec0810d1cc3819fcd36c24
SerenityBudd <serenity.budd@gmail.com>
2018-06-18 15:09:30 -0500

    read in tables and sorted the data frames by the fishcodes

--- fishCWD.R
+++ fishCWD.R
@@ -1,5 +1,38 @@
-fishdist <- read.csv("Table_Distribution.csv")
+source("libraries.R")
+
+
+fishdist <- read.csv("Table_Distribution.csv")[,-c(2:5)]
+fishdist <- fishdist[order(fishdist$Fishcode),] 
+
 fishgrowth <- read.csv("Table_Growth.csv")
+fishgrowth <- fishgrowth[order(fishgrowth$Fishcode),] 
+
 species <- read.csv("Table_LTRMP_Species_List.csv")
-fishtraits <- read.csv("Table_Miscellaneous.csv")
+species <- species[order(species$Fishcode),] 
+
+fishmisc <- read.csv("Table_Miscellaneous.csv")
+fishmisc <- fishmisc[order(fishmisc$Fishcode),] 
+
 fishreproduction <- read.csv("Table_Reproduction.csv")
+fishreproduction <- fishreproduction[order(fishreproduction$Fishcode),] 
+
+fishtraits <- read.csv("Table_Preference_and_Guild.csv")
+fishtraits <- fishtraits[order(fishtraits$Fishcode),] 
+
+dim(fishdist)
+dim(fishgrowth)
+dim(species)
+dim(fishmisc)
+dim(fishreproduction)
+dim(fishtraits)
+
+str(fishdist)
+str(fishgrowth)
+str(species)
+str(fishmisc)
+str(fishreproduction)
+str(fishtraits)
+
+identical(fishdist$Fishcode, fishgrowth$Fishcode, 
+          species$Fishcode, fishmisc$Fishcode, 
+          fishreproduction$Fishcode, fishtraits$Fishcode)
