156b188953fd75359eb613f4e3d2fd265e3da6a7
Kaija <kaijagahm@Kaijas-MacBook-Pro.local>
2018-06-18 15:29:28 -0500

    Begin work on aquatic habitat shapefiles: read in data, transform points to SpatialPoints.

---
+++ cwd.shapefiles.R
@@ -0,0 +1,14 @@
+# Working with aquatic habitat shape files 
+library(rgdal)
+
+# show the files available to read
+list.files(".", pattern='\\.shp$')
+# does the file exist?
+file.exists('p8_1989_aquahab.shp')
+
+# read the file in. Need to have all the files (.sbn, .sbx, .dbf, .prj) in the same folder, for some reason. Also, remember to *leave off the .shp extension on the shapefile!!!!!!*
+aquahab <- readOGR(dsn = ".", layer = "p8_1989_aquahab")
+glimpse(aquahab)
+
+# transform coordinates to spatial points
+points <- SpatialPoints(pool8.barcodes[,c("lon", "lat")])
