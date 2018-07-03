2e765bd39b328953de7599901ccbfe3713732fb6
Kaija <kaijagahm@Kaijas-MBP.intra.uwlax.edu>
2018-07-03 11:37:28 -0500

    Process outputs for interaction models

--- week5.sandbox.R
+++ week5.sandbox.R
@@ -6,11 +6,6 @@ load("data/new.ef.Rda")
 #Separate out the quantitative predictors and make a scatterplot matrix
 pp <- new.ef[,c("Area", "Perimeter", "sinuosity", "avg_fetch", "max_depth", "avg_depth", "tot_vol", "bath_pct", "area_gt100", "len_wetf", "pct_prm_wetf", "pct_terr_shore_wetf", "len_prm_lotic", "pct_prm_lotic", "num_lotic_outl", "len_prm_lentic", "pct_prm_lentic", "num_lentic_outl", "pct_aqveg", "pct_opwat", "len_terr", "pct_terr", "pct_aq", "len_wd", "wdl_p_m2", "num_wd", "len_revln", "pct_prm_rev", "pct_terr_shore_rev", "snag", "snagyn", "landcover_abbr", "stratum")]
 
-table(pp$stratum)
-#There are only 2 points in the tailwater zone, so let's remove these for the purpose of this analysis.
-pp <- droplevels(pp[pp$stratum != "TWZ",])
-table(pp$stratum)
-
     #none of these should have negative values
     #change negative values to NA's
     for(i in 1:ncol(pp)){
@@ -64,7 +59,7 @@ names(outputlist) <- vars
     coef = rep(NA, length(vars)),
     pval = rep(NA, length(vars))
   )
-      #fill data frame with coefficients and p values
+      # fill data frame with coefficients and p values
       for(i in 1:length(vars)){
         coef.pval$pval[i] <- outputlist[[i]]$p_value
         coef.pval$coef[i] <- coef(outputlist[[i]]$model)[2]
@@ -81,12 +76,55 @@ names(outputlist) <- vars
       coef.pval$sig05 <- ifelse(coef.pval$pval <= 0.05, ".", NA)
       head(coef.pval)
 
-#Let's look at the logit plots for the variables that were significant at a 0.05 level.
+# Let's look at the logit plots for the variables that were significant at a 0.05 level.
       indices <- coef.pval$index[!is.na(coef.pval$sig05)]
       logitplots[indices]
       densityplots[indices]
       densityplots[-indices]
       
-#Visually assess differences by stratum
-      densityplots_stratum
-    #would be nice to change this so that the densities add up to 1 within a facet. 
+      
+      
+#############################################################################
+      # Models with interactions with stratum types
+table(pp$stratum)
+      # There are only 2 points in the tailwater zone and 16 in the impounded open water zone, let's remove these for the purposes of this analysis. 
+      pp <- droplevels(pp[!pp$stratum %in% c("TWZ", "IMP-O"),])
+      table(pp$stratum)
+      # These sample sizes seem much more reasonable
+      
+      source("CWD_distribution/interact.univ.logit.regression.script.R")
+      outputlist.interact <- vector("list", length(vars))
+      
+      for(i in 1:length(vars)){
+        a <- makelogit.interact(df_source = pp, 
+                       var_of_interest = vars[[i]], 
+                       cwdfactor = "snagyn", 
+                       cwd = "snag", 
+                       varname = names(vars)[i],
+                       stratum = "stratum")
+        outputlist.interact[[i]] <- a
+      }
+      
+      #name the elements of the output list
+      names(outputlist.interact) <- vars
+      
+#Let's get these outputs sorted by type
+  #initialize lists for the output objects
+  boxplots <- vector("list", length(vars))
+  densityplots <- vector("list", length(vars))
+  modelsummaries <- vector("list", length(vars))
+  predictions <- vector("list", length(vars))
+  logitplots <- vector("list", length(vars))
+  densityplots_stratum <- vector("list", length(vars))
+      
+      #fill the lists
+      for(i in 1:length(vars)){
+        boxplots[[i]] <- outputlist[[i]]$boxplot
+        densityplots[[i]] <- outputlist[[i]]$density_plot
+        modelsummaries[[i]] <- outputlist[[i]]$model_summary
+        predictions[[i]] <- outputlist[[i]]$predictions
+        logitplots[[i]] <- outputlist[[i]]$logit_plot
+        densityplots_stratum[[i]] <- outputlist[[i]]$density_plot_stratum
+      }
+      
+      
