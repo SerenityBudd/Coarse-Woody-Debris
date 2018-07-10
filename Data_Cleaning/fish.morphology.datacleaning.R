657f4f88fb08c0a06ce10ece81314c53e280688c
SerenityBudd <serenity.budd@gmail.com>
2018-07-10 09:41:42 -0500

    commented the code for so its easier to follow

--- Data_Cleaning/fish.morphology.datacleaning.R
+++ Data_Cleaning/fish.morphology.datacleaning.R
@@ -11,21 +11,21 @@ sciname <- paste(fb$Genus, fb$Species)
 index <- fishinfo$Scientific.Name.Current %in% sciname
 fishcorrect <- fishinfo[index,]
 dim(fishcorrect)
-#132 species left
+# 132 species left, out of 152 original
 
 morpho <- morphometrics(species_list = fishcorrect$Scientific.Name.Current)
-#28 or more
 length(unique(morpho$sciname))
-
-#end up with 118 species
+# end up with 118 species
 
 badfish <- fishinfo[!index,]
 unique(badfish$Scientific.Name.Current)
+# they are all cross species
 
+# take the variables i want
 str(morpho)
+morpho_new <- select(morpho, sciname:CA)
 
-morpho_new <- dplyr::select(morpho, sciname:CA)
-
+# group by the scientific name and average all the values per species
 morpho_grouped <- morpho_new %>%
   group_by(sciname) %>%
   summarise_all(funs(mean(., na.rm=TRUE)))
@@ -34,5 +34,4 @@ morpho_grouped_complete <- morpho_grouped[complete.cases(morpho_grouped),]
 dim(morpho_grouped_complete)
 # 110 fish left
 
-fishclustercomplete <- morpho_grouped_complete
-save(fishclustercomplete, file = "data/fishclustercomplete.Rda")
+#save(morpho_grouped_complete, file = "data/morpho_grouped_complete.Rda")
