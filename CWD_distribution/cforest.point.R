8e7690482d92ac8801631af3d2df8317c48b42dc
Kaija <kaijagahm@Kaijas-MBP.intra.uwlax.edu>
2018-07-24 17:40:23 -0500

    Changed variable names, corrected error that resulted from variable name "wingdam/dyke" with a slash in it. Dropped pct_aqveg. Computed cross-validated averaged error rates from the random forest models and confirmed that all pools work properly.

--- CWD_distribution/cforest.point.R
+++ CWD_distribution/cforest.point.R
@@ -3,111 +3,215 @@
 # cforest tree on point level for larger sample size
 #========================================
 load("data/all_reduced.Rda")
-all_reduced <- all_reduced %>% dplyr::select(-c(barcode, uniq_id, Area, pct_prm_lotic, pct_prm_lentic, num_lotic_outl, num_lentic_outl, econ, pct_terr_shore_wetf, sinuosity, NEAR_TERR_CLASS_31_N.p, NEAR_FOREST_CLASS_31_N.p, year.p, stageht.p, stratum_name, snagyn, pct_area_le100, depth.p, current.p, substrt.p, trib.p)) %>% na.omit()
+all_reduced <- all_reduced %>% dplyr::select(-c(barcode, uniq_id, Area, pct_prm_lotic, pct_prm_lentic, num_lotic_outl, num_lentic_outl, econ, pct_terr_shore_wetf, sinuosity, NEAR_TERR_CLASS_31_N.p, NEAR_FOREST_CLASS_31_N.p, year.p, stageht.p, stratum_name, snagyn, pct_area_le100, depth.p, current.p, substrt.p, trib.p, pct_aqveg)) %>% na.omit()
 all <- all_reduced
-#=======================
-# Points, all pools 
-#=======================
-all <- splitup(all, 5) 
+colnames(all) <- c("stratum", "snag", "aqua_code", "pool", "perimeter", "max_depth", "avg_depth", "total_volume", "shoreline_density_index", "pct_terrestrial_shore", "pct_perimeter_wetforest", "dist_to_land", "nearest_land_class", "dist_to_forest", "nearest_forest_class", "wingdam", "revetment")
+all$snag <- factor(as.character(all$snag))
 
-vars <- colnames(all)[c(1, 3:18)]
-importances <- data.frame(
-  vars
-)
+#=========================
+# intialize functions
+#=========================
 
-for(i in 1:5){
-  pointTemp <- all[all$grp == i, 1:18]
-  
-  tree <- partykit::cforest(snag~., 
-                            data = pointTemp,
-                            ntree = 500, trace = T,
-                            perturb = list(replace = F,
-                                           fraction = 0.632))
-  vi.part <- partykit::varimp(tree)
-  importances[,i] <- vi.part
-  print(paste("Run", i, "complete"))
+# initialize function to make nicer variable importance plot
+plotnice <- function(varimps, color, title){
+  ggplot(data = varimps, aes(x = predictorvars, y = imps))+
+    geom_col(fill = color)+
+    theme_bw()+
+    theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
+          axis.title.x=element_blank())+
+    ylab("Rel. importance")+
+    ggtitle(title)+
+    theme(text = element_text(size=20))
 }
 
-impsums <- rowSums(importances)
-imps <- impsums/5
-varimps <- cbind(vars, imps) %>% as.data.frame %>% mutate(imps = as.numeric(as.character(imps))) %>% mutate(vars = reorder(vars, -imps))
+# initialize function for splitting data into cross-validation sets
+splitup <- function(df, k){
+  set.seed(1)
+  inds <- sample(rep(1:k, length = nrow(df)))
+  df$grp <- inds
+  return(df)
+}
 
-plotnice(varimps, color = "olivedrab4", pool = "all pools, points")
+#=================================
+# Points, all pools 
+#=================================
 
+# split the full data set into 5 groups
+all <- splitup(all, 5)
+
+# define predictor variables for the full data set
+predictorvars_all <- c("stratum", "aqua_code", "pool", "perimeter", "max_depth", "avg_depth", "total_volume", "shoreline_density_index", "pct_terrestrial_shore", "pct_perimeter_wetforest", "dist_to_land", "nearest_land_class", "dist_to_forest", "nearest_forest_class", "wingdam", "revetment")
+
+# initialize an empty data frame to contain variable importance values
+importances_all <- data.frame(
+  predictorvars_all
+)
+
+# intialize an empty vector to contain error rates
+ers <- rep(NA, 5)
+
+for(i in 1:5){
+  train <- all %>% dplyr::filter(grp != i) %>% dplyr::select(-grp) # train on 4/5 of the data, remove `grp` because it's not a predictor variable
+  test <- all %>% dplyr::filter(grp == i) %>% dplyr::select(-grp) # test on the remaining 1/5 of the data. Remove `grp``
+  # build a tree
+  tree <- partykit::cforest(snag~., 
+                            data = train,
+                            strata = snag,
+                            ntree = 5, trace = T,
+                            perturb = list(replace = F,
+                                           fraction = 0.632)) # weights?
+  pred_resp <- predict.cforest(tree, newdata = test, type = "response") # response predictions. weights?
+  cm <- table(true = test$snag, pred = predict(tree, newdata = test)) #confusion matrix
+  e <- (cm[1,2] + cm[2,1])/sum(cm) # calculate the error rate
+  ers[i] <- e # assign to the i'th element of ers vector
+  
+  # calculate variable importances
+  vi.part <- varimp(tree)
+  importances_all[,i] <- vi.part
+  print(paste("Run", i, "complete"))
+}
+cv_error_all <- mean(ers) # cross-validated mean error rate
+
+imps_all <- rowSums(importances_all)/5 # calculate average variable importances
+varimps_all <- cbind(predictorvars_all, imps) %>% as.data.frame %>% mutate(imps = as.numeric(as.character(imps))) %>% mutate(predictorvars_all = reorder(predictorvars_all, -imps))
+colnames(varimps_all) <- c("predictorvars", "imps")# get the data into the right format for ggplot
+
+plotnice(varimps_all, color = "olivedrab4", title = "Point RF var. importance (5fold), all pools")
+# save as rf_avg_varimp_point_all_5foldcv.png
+
+#=============================
+# Points, individual pools 
+#=============================
+
+predictorvars_pools <- c("stratum", "aqua_code", "perimeter", "max_depth", "avg_depth", "total_volume", "shoreline_density_index", "pct_terrestrial_shore", "pct_perimeter_wetforest", "dist_to_land", "nearest_land_class", "dist_to_forest", "nearest_forest_class", "wingdam", "revetment") #define vars for the pools
 
 #=============================
 # pool 4
 
-vars <- colnames(all)[c(1, 3, 5:18)]
-importances <- data.frame(
-  vars
+points_4 <- splitup(all[all$pool == 4,], 5) # split data into 5 groups
+
+# initialize an empty data frame to contain variable importance values
+importances_pool4 <- data.frame(
+  predictorvars_pools
 )
 
-points_4 <- splitup(all[all$pool == 4,], 5)
+# intialize an empty vector to contain error rates
+ers <- rep(NA, 5)
 
 for(i in 1:5){
-  pointTemp <- points_4[points_4$grp == i, c(1:3, 5:19)]
+  train <- points_4 %>% dplyr::filter(grp != i) %>% dplyr::select(-c(grp, pool)) # train on 4/5 of the data, remove `grp` because it's not a predictor variable. Also remove `pool` because now we're only in one pool. 
+  test <- points_4 %>% dplyr::filter(grp == i) %>% dplyr::select(-c(grp, pool)) # test on the remaining 1/5 of the data. Remove `grp` and `pool`.
   
+  # build a tree
   tree <- partykit::cforest(snag~., 
-                            data = pointTemp,
-                            ntree = 1000, trace = T,
+                            data = train,
+                            strata = snag,
+                            ntree = 5, trace = T,
                             perturb = list(replace = F,
-                                           fraction = 0.632))
-  vi.part <- partykit::varimp(tree)
-  importances[,i] <- vi.part
+                                           fraction = 0.632)) # weights?
+  pred_resp <- predict.cforest(tree, newdata = test, type = "response") # response predictions. weights?
+  cm <- table(true = test$snag, pred = predict(tree, newdata = test)) #confusion matrix
+  e <- (cm[1,2] + cm[2,1])/sum(cm) # calculate the error rate
+  ers[i] <- e # assign to the i'th element of ers vector
+  
+  # calculate variable importances
+  vi.part <- varimp(tree)
+  importances_pool4[,i] <- vi.part
   print(paste("Run", i, "complete"))
 }
+cv_error_pool4 <- mean(ers) # cross-validated mean error rate
 
-impsums <- rowSums(importances)
-imps <- impsums/5
-varimps <- cbind(vars, imps) %>% as.data.frame %>% mutate(imps = as.numeric(as.character(imps))) %>% mutate(vars = reorder(vars, -imps))
+imps_pool4 <- rowSums(importances_pool4)/5 # calculate average variable importances
+varimps_pool4 <- cbind(predictorvars_pools, imps_pool4) %>% as.data.frame %>% mutate(imps_pool4 = as.numeric(as.character(imps_pool4))) %>% mutate(predictorvars_pools = reorder(predictorvars_pools, -imps_pool4)) # get the data into the right format for ggplot
+colnames(varimps_pool4) <- c("predictorvars", "imps")
 
-plotnice(varimps, color = "cadetblue4", pool = "pool 4, points")
+plotnice(varimps_pool4, color = "cadetblue4", title = "Point RF var. importance (5fold), pool 4")
+# save as rf_avg_varimp_point_pool4_5foldcv.png
 
 #=============================
 # pool 8
 
-points_8 <- splitup(all[all$pool == 8,], 5)
+points_8 <- splitup(all[all$pool == 8,], 5) # split data into 5 groups
+
+# initialize an empty data frame to contain variable importance values
+importances_pool8 <- data.frame(
+  predictorvars_pools
+)
+
+# intialize an empty vector to contain error rates
+ers <- rep(NA, 5)
 
 for(i in 1:5){
-  pointTemp <- points_8[points_8$grp == i, c(1:3, 5:19)]
+  train <- points_8 %>% dplyr::filter(grp != i) %>% dplyr::select(-c(grp, pool)) # train on 4/5 of the data, remove `grp` because it's not a predictor variable. Also remove `pool` because now we're only in one pool. 
+  test <- points_8 %>% dplyr::filter(grp == i) %>% dplyr::select(-c(grp, pool)) # test on the remaining 1/5 of the data. Remove `grp` and `pool`.
   
+  # build a tree
   tree <- partykit::cforest(snag~., 
-                            data = pointTemp,
-                            ntree = 1000, trace = T,
+                            data = train,
+                            strata = snag,
+                            ntree = 5, trace = T,
                             perturb = list(replace = F,
-                                           fraction = 0.632))
-  vi.part <- partykit::varimp(tree)
-  importances[,i] <- vi.part
+                                           fraction = 0.632)) # weights?
+  pred_resp <- predict.cforest(tree, newdata = test, type = "response") # response predictions. weights?
+  cm <- table(true = test$snag, pred = predict(tree, newdata = test)) #confusion matrix
+  e <- (cm[1,2] + cm[2,1])/sum(cm) # calculate the error rate
+  ers[i] <- e # assign to the i'th element of ers vector
+  
+  # calculate variable importances
+  vi.part <- varimp(tree)
+  importances_pool8[,i] <- vi.part
   print(paste("Run", i, "complete"))
 }
+cv_error_pool8 <- mean(ers) # cross-validated mean error rate
 
-impsums <- rowSums(importances)
-imps <- impsums/5
-varimps <- cbind(vars, imps) %>% as.data.frame %>% mutate(imps = as.numeric(as.character(imps))) %>% mutate(vars = reorder(vars, -imps))
+imps_pool8 <- rowSums(importances_pool8)/5 # calculate average variable importances
+varimps_pool8 <- cbind(predictorvars_pools, imps_pool8) %>% as.data.frame %>% mutate(imps_pool8 = as.numeric(as.character(imps_pool8))) %>% mutate(predictorvars_pools = reorder(predictorvars_pools, -imps_pool8)) # get the data into the right format for ggplot
+colnames(varimps_pool8) <- c("predictorvars", "imps")
 
-plotnice(varimps, color = "brown", pool = "pool 8, points")
+plotnice(varimps_pool8, color = "brown", title = "Point RF var. importance (5fold), pool 8")
+# save as rf_avg_varimp_point_pool8_5foldcv.png
 
 #=============================
 # pool 13
 
-points_13 <- splitup(all[all$pool == 13,], 5)
+points_13 <- splitup(all[all$pool == 13,], 5) # split data into 5 groups
+
+# initialize an empty data frame to contain variable importance values
+importances_pool13 <- data.frame(
+  predictorvars_pools
+)
+
+# intialize an empty vector to contain error rates
+ers <- rep(NA, 5)
 
 for(i in 1:5){
-  pointTemp <- points_13[points_13$grp == i, c(1:3, 5:19)]
+  train <- points_13 %>% dplyr::filter(grp != i) %>% dplyr::select(-c(grp, pool)) # train on 4/5 of the data, remove `grp` because it's not a predictor variable. Also remove `pool` because now we're only in one pool. 
+  test <- points_13 %>% dplyr::filter(grp == i) %>% dplyr::select(-c(grp, pool)) # test on the remaining 1/5 of the data. Remove `grp` and `pool`.
   
+  # build a tree
   tree <- partykit::cforest(snag~., 
-                            data = pointTemp,
-                            ntree = 1000, trace = T,
+                            data = train,
+                            strata = snag,
+                            ntree = 5, trace = T,
                             perturb = list(replace = F,
-                                           fraction = 0.632))
-  vi.part <- partykit::varimp(tree)
-  importances[,i] <- vi.part
+                                           fraction = 0.632)) # weights?
+  pred_resp <- predict.cforest(tree, newdata = test, type = "response") # response predictions. weights?
+  cm <- table(true = test$snag, pred = predict(tree, newdata = test)) #confusion matrix
+  e <- (cm[1,2] + cm[2,1])/sum(cm) # calculate the error rate
+  ers[i] <- e # assign to the i'th element of ers vector
+  
+  # calculate variable importances
+  vi.part <- varimp(tree)
+  importances_pool13[,i] <- vi.part
   print(paste("Run", i, "complete"))
 }
+cv_error_pool13 <- mean(ers) # cross-validated mean error rate
 
-impsums <- rowSums(importances)
-imps <- impsums/5
-varimps <- cbind(vars, imps) %>% as.data.frame %>% mutate(imps = as.numeric(as.character(imps))) %>% mutate(vars = reorder(vars, -imps))
+imps_pool13 <- rowSums(importances_pool13)/5 # calculate average variable importances
+varimps_pool13 <- cbind(predictorvars_pools, imps_pool13) %>% as.data.frame %>% mutate(imps_pool13 = as.numeric(as.character(imps_pool13))) %>% mutate(predictorvars_pools = reorder(predictorvars_pools, -imps_pool13)) # get the data into the right format for ggplot
+colnames(varimps_pool13) <- c("predictorvars", "imps")
 
-plotnice(varimps, color = "darkblue", pool = "pool 13, points")
+plotnice(varimps_pool13, color = "darkblue", title = "Point RF var. importance (5fold), pool 13")
+# save as rf_avg_varimp_point_pool13_5foldcv.png
+
+
